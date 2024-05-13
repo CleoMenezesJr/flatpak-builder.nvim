ef247b8a27e43a21bb177887da48e500f65032c0
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2024-05-12 23:27:45 -0400

    initial commit

--- .luacheckrc
+++
@@ -1,4 +0,0 @@
-globals = { "vim", "MiniTest" }
-max_line_length = false
-
-exclude_files = { "deps" }
--- plugin/flatpak-builder.lua
+++ plugin/flatpak-builder.lua
@@ -4,8 +4,8 @@
 -- end
 --
 -- _G.FlatpakBuilderLoaded = true
---
--- -- Useful if you want your plugin to be compatible with older (<0.7) neovim versions
+
+-- Useful if you want your plugin to be compatible with older (<0.7) neovim versions
 -- if vim.fn.has("nvim-0.7") == 0 then
 --     vim.cmd("command! FlatpakBuilder lua require('flatpak-builder').toggle()")
 -- else
@@ -14,58 +14,63 @@
 --     end, {})
 -- end
 -- print("hello world")
+
 M = {}
 ManifestoData = {}
 FlatpakSpawn = os.execute("which flatpak-spawn")
 
 function M.list_files_with_extensions()
-    local manifesto_candidates = {}
-    for file in io.popen([[ls | grep -E '.(json|ymal|yml)$']]):lines() do
-        table.insert(manifesto_candidates, file)
-    end
+  local manifesto_candidates = {}
+  for file in io.popen([[ls | grep -E '.(json|ymal|yml)$']]):lines() do
+    table.insert(manifesto_candidates, file)
+  end
 
-    M.get_file_with_reversed_domain(manifesto_candidates)
+  M.get_file_with_reversed_domain(manifesto_candidates)
 end
 
 function M.get_file_with_reversed_domain(candidates)
-    for _, file in ipairs(candidates) do
-        local last_dot_index = file:match(".*()%.")
-        local file_without_extension = file:sub(1, last_dot_index - 1)
+  for _, file in ipairs(candidates) do
+    local last_dot_index = file:match(".*()%.")
+    local file_without_extension = file:sub(1, last_dot_index - 1)
 
-        local is_reversed_domain_name =
-            string.match(file_without_extension, "^([a-zA-Z0-9-]+)%.([a-zA-Z0-9-]+)%.([a-zA-Z]+)$")
+    local is_reversed_domain_name =
+      string.match(file_without_extension, "^([a-zA-Z0-9-]+)%.([a-zA-Z0-9-]+)%.([a-zA-Z]+)$")
 
-        if is_reversed_domain_name then
-            ManifestoData = { filename = file_without_extension, manifesto = file }
-        end
+    if is_reversed_domain_name then
+      ManifestoData = { filename = file_without_extension, manifesto = file }
     end
-    print(ManifestoData.filename)
-    print(ManifestoData.manifesto)
+  end
+  print(ManifestoData.filename)
+  print(ManifestoData.manifesto)
+end
+
+function M.setup()
+  M.list_files_with_extensions()
 end
 
 function M.build()
-    local command = "flatpak run org.flatpak.Builder build "
-        .. ManifestoData.filename
-        .. " --user --install --force-clean"
-    if FlatpakSpawn == 0 then
-        command = "flatpak-spawn --host " .. command
-    end
+  local command = "flatpak run org.flatpak.Builder build "
+    .. ManifestoData.filename
+    .. " --user --install --force-clean"
+  if FlatpakSpawn == 0 then
+    command = "flatpak-spawn --host " .. command
+  end
 
-    os.execute(command)
+  os.execute(command)
 end
 
 function M.run()
-    local command = "flatpak run " .. ManifestoData.manifesto
-    if FlatpakSpawn == 0 then
-        command = "flatpak-spawn --host " .. command
-    end
+  local command = "flatpak run " .. ManifestoData.manifesto
+  if FlatpakSpawn == 0 then
+    command = "flatpak-spawn --host " .. command
+  end
 
-    os.execute(command)
+  os.execute(command)
 end
 
 function M.build_and_run()
-    M.build()
-    M.run()
+  M.build()
+  M.run()
 end
 
 return M
